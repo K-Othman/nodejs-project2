fe81410810cc4f65bb92c5642cf5518b85c75d24
K-Othman <karim_othman88@hotmail.com>
2024-03-06 13:45:35 +0200

    creating books query

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,27 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Apple@1515",
+  database: "test",
+});
+
+app.get("/", (req, res) => {
+  res.json("Hello This Is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  console.log(q);
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
 app.listen(8800, () => {
   console.log("Connected to backend!");
