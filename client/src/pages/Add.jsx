126ee27aa6ac916920fe7b7a8f2356b12317406d
K-Othman <karim_othman88@hotmail.com>
2024-03-13 23:50:00 +0000

    working on the client side

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,5 +1,48 @@
+import { useState } from "react";
+
 const Add = () => {
-  return <div>Add</div>;
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  console.log(book);
+
+  return (
+    <div className="form">
+      <h1>Add New Book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="desc"
+        onChange={handleChange}
+        name="desc"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      />
+      <button>Add</button>
+    </div>
+  );
 };
 
 export default Add;
